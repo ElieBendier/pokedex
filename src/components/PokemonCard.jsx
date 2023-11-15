9f5efe4923e5516cff294e64bda50f9fb02012fc
ElieBendier <elie.bendier@netc.eu>
2023-11-15 20:28:19 +0100

    props done

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,17 +1,7 @@
 import styles from '../css/PokemonCard.module.css'
-const pokemonList = [
-    {
-        name: "Bulbasaur",
-        imgSrc:
-            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-        name: "Mew",
-    },
-];
 
-function PokemonCard() {
-    let pokemon = pokemonList[0];
+
+function PokemonCard({ pokemon }) {
     return (
         <figure className={styles.card}>
             {pokemon.imgSrc != null ? <img className={styles.cardImg} src={pokemon.imgSrc} alt={pokemon.name} /> : <p>???</p>}
